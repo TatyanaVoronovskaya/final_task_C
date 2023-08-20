7b3694af69b683117868795ab54a7331228043ad
Tatyana <vta278@mail.ru>
2023-08-20 19:24:28 +0300

    добавлены методы ввода числа и проверка положительности

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует
+//  новый массив из строк, длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+int ReadInt(string message)               // ввод количеста элементов массива
+{
+    System.Console.Write($"{message} > ");
+    string newString = Console.ReadLine();
+    if (int.TryParse(newString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+    System.Console.WriteLine("Вы ввели не целое число");
+    Environment.Exit(0);
+    return 0;
+}
+
+bool NPositive(int n)                  // проверка положительности числа
+{
+    if (n <= 0)
+    {
+        System.Console.WriteLine($"Вы ввели число {n}, которое < или = 0");
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
